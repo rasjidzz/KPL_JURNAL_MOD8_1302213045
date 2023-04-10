75e30ce16f920489b8473edc8ad139519b3919fc
rasjidzz <mrisjads@gmail.com>
2023-04-10 08:24:51 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ KPL_JURNAL_MOD8_1302213045/BankTransferConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace KPL_JURNAL_MOD8_1302213045
+{
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; }
+        List<string> methods = new List<string>() { "RTO(real - time)", "SKN", "RTGS", "BI FAST" };
+        public transfer transfer { get; set; }
+        public confirmation confirmation { get; set; }
+
+        public BankTransferConfig()
+        {
+            this.lang = "en";
+            this.transfer = new transfer();
+            this.transfer.threshold = 25000000;
+            this.transfer.low_fee = 6500;
+            this.transfer.high_fee = 15000;
+            this.confirmation = new confirmation();
+            this.confirmation.en = "yes";
+            this.confirmation.id = "ya";
+        }
+        public BankTransferConfig readJSON()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string jsonString = File.ReadAllText(path + "/bank_transfer_config.json");
+            return JsonSerializer.Deserialize<BankTransferConfig>(jsonString);
+        }
+        
+    }
+    public class transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set;}
+    }
+    public class confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+    public class methods
+    {
+
+    }
+}
--- KPL_JURNAL_MOD8_1302213045/Program.cs
+++ KPL_JURNAL_MOD8_1302213045/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("First innit");
+
+using KPL_JURNAL_MOD8_1302213045;
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig bank = new BankTransferConfig();
+        bank.readJSON();
+    }
+}
